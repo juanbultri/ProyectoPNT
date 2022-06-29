287dfb55e716be58a83f5ed68c1ee7fe45098611
Leandro <leandrotaddeo03@gmail.com>
2022-06-29 01:16:49 -0300

    agrega funcion de javaScript

--- ProyectoGym/wwwroot/js/site.js
+++ ProyectoGym/wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function listenerPerfil() {
+    let perfil = document.getElementById("Perfil");
+    let group_plan = document.getElementById("grupo_plan");
+    let plan = document.getElementById("Plan");
+
+    if (perfil.value != 2) {
+        group_plan.style.display = "none";
+        plan.value = "";
+    } else {
+        group_plan.style.display = "";
+    }
+}
