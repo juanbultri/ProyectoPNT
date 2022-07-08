691ae507139b8aae8917286d41d7ec65458ff22f
Cesar Delgadillo <cesardelgadillo@gmail.com>
2022-07-07 21:47:48 -0300

    Se corrigen estilos en Actividades y home

--- ProyectoGym/Models/Actividad.cs
+++ ProyectoGym/Models/Actividad.cs
@@ -10,13 +10,18 @@ namespace ProyectoGym.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Debe especificar un nombre")]
         public string Nombre { get; set; }
 
         [EnumDataType(typeof(Dias))]
+        [Required(ErrorMessage = "Debe especificar el día de la actividad")]
         public Dias Dia { get; set; }
 
+        [Required(ErrorMessage = "Debe especificar la hora")]
+        [DataType(DataType.Time)]
         public string Hora { get; set; }
 
+        [Required(ErrorMessage = "Debe especificar la duración")]
         public int Duracion { get; set; }
 
     }
