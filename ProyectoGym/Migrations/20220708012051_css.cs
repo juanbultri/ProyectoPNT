7255607ce959dde942296af512c8876c62fc5dce
Bultri <juaneliseo.bultri@abbott.com>
2022-07-07 22:48:41 -0300

    Se agrega estilos de usuario y actividades de usuario

---
+++ ProyectoGym/Migrations/20220708012051_css.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProyectoGym.Migrations
+{
+    public partial class css : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Plan",
+                table: "Usuarios",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Usuarios",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombres",
+                table: "Usuarios",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Apellidos",
+                table: "Usuarios",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Detalle",
+                table: "Rutinas",
+                maxLength: 300,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Actividades",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Hora",
+                table: "Actividades",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Plan",
+                table: "Usuarios",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Usuarios",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombres",
+                table: "Usuarios",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Apellidos",
+                table: "Usuarios",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Detalle",
+                table: "Rutinas",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 300);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Actividades",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Hora",
+                table: "Actividades",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
--- ProyectoGym/Models/Usuario.cs
+++ ProyectoGym/Models/Usuario.cs
@@ -9,20 +9,20 @@ namespace ProyectoGym.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        
+        [Required(ErrorMessage = "Debe especificar un nombre")]
         public string Nombres { get; set; }
-        
+        [Required(ErrorMessage = "Debe especificar un apellido")]
         public string Apellidos { get; set; }
-
+        [Required(ErrorMessage = "Debe especificar un nombre de usuario")]
         [Display(Name = "Nombre de usuario")]
         public string NombreUsuario { get; set; }
-
+        [Required(ErrorMessage = "Debe especificar una contraseña")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
-
+        [Required(ErrorMessage = "Debe especificar un tipo de perfil")]
         [EnumDataType(typeof(Perfil))]
         public Perfil Perfil { get; set; }
-
+        [Required(ErrorMessage = "Debe especificar un Plan")]
         [EnumDataType(typeof(Plan))]
         public Plan? Plan { get; set; }
     }
